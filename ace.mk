2d2d8a37d6117fd378fe22c11f10f46025d44e09
Twisted Playground <twistedumbrella@gmail.com>
2011-12-23 15:01:30 -0500

    Cleanup Device Properties

--- ace.mk
+++ ace.mk
@@ -186,7 +186,7 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
 
 # media profiles and capabilities spec
-$(call inherit-product, device/htc/ace/media_a1026.mk)
+$(call inherit-product, device/htc/ace/media_htcaudio.mk)
 
 # stuff common to all HTC phones
 $(call inherit-product, device/htc/common/common.mk)
