b52bae75daef2c024c7cf4a85d629b2543017ce7
Twisted Playground <twistedumbrella@gmail.com>
2011-12-31 14:01:23 -0500

    TARGET_USE_CUSTOM_LUN_FILE_PATH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,7 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
 BOARD_FLASH_BLOCK_SIZE := 262144
 
 TARGET_SPECIFIC_HEADER_PATH := device/htc/ace/include
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 TARGET_PREBUILT_KERNEL := device/htc/ace/kernel/kernel
