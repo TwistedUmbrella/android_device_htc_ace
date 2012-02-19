37d4d225045850de51457b193e4bbfe85b4b7e88
Twisted <twistedumbrella@gmail.com>
2012-02-19 10:10:08 -0500

    Complete redesign of proprietary Google vendor packages

--- ace.mk
+++ ace.mk
@@ -22,8 +22,6 @@ PRODUCT_COPY_FILES += \
 
 ## (2) Also get non-open-source GSM-specific aspects if available
 $(call inherit-product-if-exists, vendor/htc/ace/ace-vendor.mk)
-$(call inherit-product-if-exists, vendor/twisted/twisted-vendor.mk)
-$(call inherit-product-if-exists, vendor/twisted/google-vendor.mk)
 
 ## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
 PRODUCT_PROPERTY_OVERRIDES += \
